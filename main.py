b45e2ea6774732c5c6f5305cc2664e7caa84c6e9
Renat Shabanov <multizloy@gmail.com>
2024-07-14 20:03:30 +0300

    end

--- main.py
+++ main.py
@@ -19,11 +19,20 @@ class Game:
             self.clock.tick(FPS)
             self.events()
             self.draw()
+        else:
+            self.endScreen()
 
     def draw(self):
         self.screen.fill(BGCOLOUR)
         self.board.draw(self.screen)
         pygame.display.flip()
+
+    def checkWin(self):
+        for row in self.board.boardList:
+            for tile in row:
+                if tile.type != "X" and not tile.revealed:
+                    return False
+        return True
 
     def events(self):
         for event in pygame.event.get():
@@ -49,11 +58,29 @@ class Game:
                                     elif tile.type == "X":
                                         tile.revealed == True
                             self.playing = False
+
                 if event.button == 3:
                     if not self.board.boardList[mx][my].revealed:
                         self.board.boardList[mx][my].flagged = not self.board.boardList[
                             mx
                         ][my].flagged
+
+                if self.checkWin():
+                    self.win = True
+                    self.playing = False
+                    for row in self.board.boardList:
+                        for tile in row:
+                            if not tile.revealed:
+                                tile.flagged = True
+
+    def endScreen(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit(0)
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    return
 
 
 game = Game()
