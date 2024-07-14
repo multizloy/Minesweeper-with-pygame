0c2f2cb7e35001d42c47db534a51060d80f14e2b
Renat Shabanov <multizloy@gmail.com>
2024-07-14 11:15:29 +0300

    создаем Minesweeper с Pygame

---
+++ main.py
@@ -0,0 +1,37 @@
+import pygame
+from settings import *
+from sprites import *
+
+
+class Game:
+    def __init__(self) -> None:
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(title)
+        self.clock = pygame.time.Clock * ()
+
+    def new(self):
+        pass
+
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.draw()
+
+    def draw(self):
+        self.screen.fill(BGCOLOUR)
+
+        pygame.display.flip()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit(0)
+
+
+game = Game()
+while True:
+    game.new()
+    game.run()
